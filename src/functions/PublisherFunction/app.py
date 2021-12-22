01d6ca79de785c9aaaab6fb20fe3ba2deef638be
Atharva21 <atharva.m.mulay@gmail.com>
2021-12-23 01:25:04 +0530

    adds pipfile to src

--- src/functions/PublisherFunction/app.py
+++ src/functions/PublisherFunction/app.py
@@ -1,4 +1,7 @@
-import json, boto3, uuid, os
+import json
+import boto3
+import uuid
+import os
 
 _queue_name = os.getenv('QUEUE_NAME')
 _bucket_name = os.getenv('BUCKET_NAME')
@@ -7,17 +10,18 @@ s3 = boto3.resource('s3')
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName=_queue_name)
 
+
 def main(event, _):
-	body = json.loads(event['body'])
-	uid = uuid.uuid4()
-	s3object = s3.Object(_bucket_name, f'{uid}.json')
-	s3object.put(
-		Body=(bytes(json.dumps(body).encode('UTF-8')))
-	)
-	queue.send_message(MessageBody=json.dumps({
-		'file_name': f'{uid}.json'
-	}))
-	return {
-		'statusCode': 200,
-		'body': 'published message to SQS'
-	}
+    body = json.loads(event['body'])
+    filename = f'{uuid.uuid4()}.json'
+    s3object = s3.Object(_bucket_name, filename)
+    s3object.put(
+        Body=(bytes(json.dumps(body).encode('UTF-8')))
+    )
+    queue.send_message(MessageBody=json.dumps({
+        'file_name': filename
+    }))
+    return {
+        'statusCode': 200,
+        'body': 'published message to SQS'
+    }
