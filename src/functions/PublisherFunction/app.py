1e7242b657f2682157b62a8e5c72c6eabbda2623
Atharva21 <atharva.m.mulay@gmail.com>
2021-12-24 23:13:50 +0530

    adds logging and metrics

--- src/functions/PublisherFunction/app.py
+++ src/functions/PublisherFunction/app.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import uuid
 import os
+from aws_lambda_powertools import Logger, Metrics, Tracer
 
 _queue_name = os.getenv('QUEUE_NAME')
 _bucket_name = os.getenv('BUCKET_NAME')
@@ -10,8 +11,16 @@ s3 = boto3.resource('s3')
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName=_queue_name)
 
+logger = Logger()
+tracer = Tracer()
+metrics = Metrics()
 
+
+@metrics.log_metrics(capture_cold_start_metric=True)
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
 def main(event, _):
+    logger.info('hello from logger')
     body = json.loads(event['body'])
     filename = f'{uuid.uuid4()}.json'
     s3object = s3.Object(_bucket_name, filename)
