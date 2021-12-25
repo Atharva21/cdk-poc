6b4dc6a58131d6b18cfab530710d7780f81ae135
Atharva21 <atharva.m.mulay@gmail.com>
2021-12-25 11:43:12 +0530

    adds logging

--- src/functions/PublisherFunction/app.py
+++ src/functions/PublisherFunction/app.py
@@ -1,7 +1,9 @@
 import json
+from typing import Any, Dict
 import boto3
 import uuid
 import os
+from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools import Logger, Metrics, Tracer
 
 _queue_name = os.getenv('QUEUE_NAME')
@@ -19,18 +21,28 @@ metrics = Metrics()
 @metrics.log_metrics(capture_cold_start_metric=True)
 @logger.inject_lambda_context
 @tracer.capture_lambda_handler
-def main(event, _):
-    logger.info('hello from logger')
-    body = json.loads(event['body'])
-    filename = f'{uuid.uuid4()}.json'
-    s3object = s3.Object(_bucket_name, filename)
-    s3object.put(
-        Body=(bytes(json.dumps(body).encode('UTF-8')))
-    )
-    queue.send_message(MessageBody=json.dumps({
-        'file_name': filename
-    }))
-    return {
-        'statusCode': 200,
-        'body': 'published message to SQS'
-    }
+def main(event: Dict[str, Any], context: LambdaContext):
+    request_id = context.aws_request_id or uuid.uuid4()
+    logger.structure_logs(append=True, requestId=request_id)
+    try:
+        body = json.loads(event['body'])
+        logger.info(f'publisher lambda called with requestId {request_id}')
+        filename = f'{uuid.uuid4()}.json'
+        s3object = s3.Object(_bucket_name, filename)
+        s3object.put(
+            Body=(bytes(json.dumps(body).encode('UTF-8')))
+        )
+        logger.info(f'put {filename} into s3 bucket.')
+        queue.send_message(MessageBody=json.dumps({
+            'file_name': filename
+        }))
+        logger.info(f'sent {filename} to sqs')
+        return {
+            'statusCode': 200,
+            'body': 'published message to SQS'
+        }
+    except Exception:
+        return {
+            'statusCode': 500,
+            'body': 'something went wrong'
+        }
--- src/functions/SubscriberFunction/app.py
+++ src/functions/SubscriberFunction/app.py
@@ -1,4 +1,9 @@
-import json, boto3, os
+import json
+import boto3
+import os
+import uuid
+from aws_lambda_powertools import Logger, Metrics, Tracer
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
 _table_name = os.getenv('TABLE_NAME')
 _bucket_name = os.getenv('BUCKET_NAME')
@@ -7,28 +12,48 @@ s3 = boto3.resource('s3')
 ddb = boto3.resource('dynamodb')
 table = ddb.Table(_table_name)
 
-def main(event, _):
-	message_body = json.loads(event['Records'][0]['body'])
-	s3object = s3.Object(_bucket_name, message_body['file_name'])
-	file_content = s3object.get()['Body'].read().decode('utf-8')
-	json_content = json.loads(file_content)
+logger = Logger()
+tracer = Tracer()
+metrics = Metrics()
 
-	common_keys = ['siteId', 'origin', 'country']
-	item_keys = ['barcode', 'linkedItemId', 'stockOnHand', 'availableForSale', 'lastUpdated', 'lastModified', 'updateReason', 'olaCode']
-	items = []
-	for item in json_content['items']:
-		filtered_item = {}
-		for key in common_keys:
-			filtered_item[key] = json_content[key]
-		for key in item_keys:
-			filtered_item[key] = item[key]
-		items.append(filtered_item)
 
-	with table.batch_writer() as batch:
-		for item in items:
-			batch.put_item(Item=item)
+@metrics.log_metrics(capture_cold_start_metric=True)
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
+def main(event, context: LambdaContext):
+    request_id = context.aws_request_id or uuid.uuid4()
+    logger.structure_logs(append=True, requestId=request_id)
+    try:
+        logger.info(f'subscriber lambda called with requestId {request_id}')
+        message_body = json.loads(event['Records'][0]['body'])
+        logger.info(f'sqs message body: {message_body}')
+        s3object = s3.Object(_bucket_name, message_body['file_name'])
+        file_content = s3object.get()['Body'].read().decode('utf-8')
+        json_content = json.loads(file_content)
+        logger.info(f'json content read from s3 object: {json_content}')
 
-	return {
-		'statusCode': 200,
-		'body': 'fetched content from s3 and stored in ddb'
-	}
+        common_keys = ['siteId', 'origin', 'country']
+        item_keys = ['barcode', 'linkedItemId', 'stockOnHand', 'availableForSale',
+                     'lastUpdated', 'lastModified', 'updateReason', 'olaCode']
+        items = []
+        for item in json_content['items']:
+            filtered_item = {}
+            for key in common_keys:
+                filtered_item[key] = json_content[key]
+            for key in item_keys:
+                filtered_item[key] = item[key]
+            items.append(filtered_item)
+        logger.info(f'number of items to be written in ddb: {len(items)}')
+        with table.batch_writer() as batch:
+            for item in items:
+                batch.put_item(Item=item)
+        logger.info(f'wrote {len(items)} items to ddb table')
+        return {
+            'statusCode': 200,
+            'body': 'fetched content from s3 and stored in ddb'
+        }
+    except Exception:
+        return {
+            'statusCode': 500,
+            'body': 'something went wrong'
+        }
