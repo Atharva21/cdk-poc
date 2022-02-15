e42f18bf3281aa2a08afc1477ab9b7458e735520
Atharva21 <atharva.m.mulay@gmail.com>
2022-02-15 19:23:14 +0530

    testing with multiple files

--- src/functions/ProducerFunction/app.py
+++ src/functions/ProducerFunction/app.py
@@ -5,12 +5,13 @@ import uuid
 import os
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools import Logger, Metrics, Tracer
+from .foo import Foo
 
-_queue_name = os.getenv('QUEUE_NAME')
-_bucket_name = os.getenv('BUCKET_NAME')
+_queue_name = os.getenv("QUEUE_NAME")
+_bucket_name = os.getenv("BUCKET_NAME")
 
-_s3 = boto3.resource('s3')
-_sqs = boto3.resource('sqs')
+_s3 = boto3.resource("s3")
+_sqs = boto3.resource("sqs")
 _queue = _sqs.get_queue_by_name(QueueName=_queue_name)
 
 logger = Logger()
@@ -26,28 +27,19 @@ def main(event: Dict[str, Any], context: LambdaContext):
     logger.structure_logs(append=True, requestId=request_id)
     try:
         # write body to s3 bucket
-        body = json.loads(event['body'])
-        logger.info(f'producer lambda called with requestId {request_id}')
-        filename = f'{uuid.uuid4()}.json'
+        logger.info(Foo)
+        body = json.loads(event["body"])
+        logger.info(f"producer lambda called with requestId {request_id}")
+        filename = f"{uuid.uuid4()}.json"
         s3object = _s3.Object(_bucket_name, filename)
-        s3object.put(
-            Body=(bytes(json.dumps(body).encode('UTF-8')))
-        )
-        logger.info(f'put {filename} into s3 bucket.')
+        s3object.put(Body=(bytes(json.dumps(body).encode("UTF-8"))))
+        logger.info(f"put {filename} into s3 bucket.")
 
         # send filename to sqs.
-        _queue.send_message(MessageBody=json.dumps({
-            'file_name': filename
-        }))
-        logger.info(f'sent {filename} to sqs')
+        _queue.send_message(MessageBody=json.dumps({"file_name": filename}))
+        logger.info(f"sent {filename} to sqs")
 
-        return {
-            'statusCode': 200,
-            'body': 'published message to SQS'
-        }
+        return {"statusCode": 200, "body": "published message to SQS"}
     except Exception as e:
         logger.error(str(e))
-        return {
-            'statusCode': 500,
-            'body': 'something went wrong'
-        }
+        return {"statusCode": 500, "body": "something went wrong"}
